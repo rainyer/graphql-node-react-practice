51686c259eb3eedc6d3133ad00be4e4f888c6da4
Rainyer Santiago <rainyer@sonatalab.com>
2019-09-20 00:05:32 -0300

    fetching launches data

--- schema.js
+++ schema.js
@@ -15,7 +15,7 @@ const LaunchType = new GraphQLObjectType({
     flight_number: { type: GraphQLInt },
     mission_name: { type: GraphQLString },
     launch_year: { type: GraphQLString },
-    launch_date_local: { type: GraphQLInt },
+    launch_date_local: { type: GraphQLString },
     launch_success: { type: GraphQLBoolean },
     rocket: { type: RocketType }
   })
@@ -40,6 +40,39 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         return axios
           .get('https://api.spacexdata.com/v3/launches')
+          .then(res => res.data)
+          .catch(err => console.log(err))
+      }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then(res => res.data)
+          .catch(err => console.log(err))
+      }
+    },
+    rockets: {
+      type: GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/rockets')
+          .then(res => res.data)
+          .catch(err => console.log(err))
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        rocket_id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
           .then(res => res.data)
           .catch(err => console.log(err))
       }
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
+const cors = require('cors')
 
 const app = express()
 
 const PORT = process.ENV || 5000
+
+app.use(cors())
 
 app.use(
   '/graphql',
